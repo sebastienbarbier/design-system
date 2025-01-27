0af573bc25c2ad47441efd1bab022bdff7c23ac0
Sébastien Barbier <contact@sebastienbarbier.com>
2025-01-27 22:01:35 +0100

    Fix build

--- vite.config.js
+++ vite.config.js
@@ -5,47 +5,50 @@ import dts from "vite-plugin-dts";
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import scss from 'rollup-plugin-scss';
 
-export default defineConfig({
-  build: {
-    target: "modules",
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: [
-        resolve(__dirname, "src/index.ts"),
-        resolve(__dirname, "src/style.scss"),
-        resolve(__dirname, "src/baseline.scss"),
-      ],
-      formats: ["es"],
-      name: "@sebastienbarbier/design-system",
-      // the proper extensions will be added
-      fileName: "[name]",
-    },
-    cssCodeSplit: true,
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: [""],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          // vue: 'Vue',
+export default defineConfig(({ mode }) => {
+  const isLibraryBuild = process.env.BUILD_STORYBOOK !== 'true'; // Check the environment variable
+  return{
+    build: {
+      target: "modules",
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: [
+          resolve(__dirname, "src/index.ts"),
+          resolve(__dirname, "src/style.scss"),
+          resolve(__dirname, "src/baseline.scss"),
+        ],
+        formats: ["es"],
+        name: "@sebastienbarbier/design-system",
+        // the proper extensions will be added
+        fileName: "[name]",
+      },
+      cssCodeSplit: true,
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: [""],
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            // vue: 'Vue',
+          },
         },
       },
     },
-  },
-  optimizeDeps: {},
-  plugins: [
-    // viteStaticCopy({
-    //   targets: [
-    //     {
-    //       src: "LICENSE",
-    //       dest: "",
-    //     },
-    //   ],
-    // }),
-    dts({
-      rollupTypes: true,
-    }),
-  ],
+    optimizeDeps: {},
+    plugins: [
+      // viteStaticCopy({
+      //   targets: [
+      //     {
+      //       src: "LICENSE",
+      //       dest: "",
+      //     },
+      //   ],
+      // }),
+      isLibraryBuild && dts({
+        rollupTypes: true,
+      }),
+    ],
+  };
 });
